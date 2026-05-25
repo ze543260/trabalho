75dd0946bfcf480f4ee34daf678f63ec45b0e49f
José Vitor gomes Nascimento <130457574+ze543260@users.noreply.github.com>
2026-05-25 20:27:26 +0100

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,26 @@
+ 
+
+
+> Open this page at [https://ze543260.github.io/trabalho/](https://ze543260.github.io/trabalho/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/ze543260/trabalho** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/ze543260/trabalho** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ assets.ts
@@ -0,0 +1,172 @@
+namespace Assets {
+    export const baristaBase = img`
+        . . . . . f f f f f f . . . . .
+        . . . . f e e e e e e f . . . .
+        . . . f e e d d d d e e f . . .
+        . . . f f d f d d f d f f . . .
+        . . . f d d d d d d d d f . . .
+        . . . . f d d d d d d f . . . .
+        . . . . f f f f f f f f . . . .
+        . . . f 1 1 1 1 1 1 1 1 f . . .
+        . . f d 1 e e e e e e 1 d f . .
+        . . f d 1 e e e e e e 1 d f . .
+        . . f f f e e e e e e f f f . .
+        . . . . f e e e e e e f . . . .
+        . . . . f e e e f e e e f . . .
+        . . . . f 8 8 8 f 8 8 8 f . . .
+        . . . f f f f . . f f f f . . .
+        . . . . . . . . . . . . . . . .
+    `;
+
+    export const customerBase = img`
+        . . . . . f f f f . . . . . . .
+        . . . . f f f f f f . . . . . .
+        . . . . f d d d d f . . . . . .
+        . . . . f d d d d f . . . . . .
+        . . . . . f d d f . . . . . . .
+        . . . . . 1 1 1 1 . . . . . . .
+        . . . . 1 1 1 1 1 1 . . . . . .
+        . . . 1 1 1 1 1 1 1 1 . . . . .
+        . . . 1 1 1 1 1 1 1 1 . . . . .
+        . . . 1 1 1 1 1 1 1 1 . . . . .
+        . . . . 1 1 1 1 1 1 . . . . . .
+        . . . . 1 1 . . 1 1 . . . . . .
+        . . . . 1 1 . . 1 1 . . . . . .
+        . . . . 1 1 . . 1 1 . . . . . .
+        . . . . f f . . f f . . . . . .
+        . . . . . . . . . . . . . . . .
+    `;
+
+    export const grinder = img`
+        . . . f f f f f f f f f . . . .
+        . . f 9 9 9 9 9 9 9 9 9 f . . .
+        . . f 9 1 9 9 9 9 9 1 9 f . . .
+        . . f 9 9 9 6 6 9 9 9 9 f . . .
+        . . f 9 9 9 6 6 9 9 9 9 f . . .
+        . . f 9 9 9 9 9 9 9 9 9 f . . .
+        . . . f f f f f f f f f . . . .
+        . f e e e e e e e e e e e f . .
+        . f e 1 e e e e e e e 1 e f . .
+        . f e e e e e e e e e e e f . .
+        . f e e e e e e e e e e e f . .
+        . f e e 1 1 e e 1 1 e e e f . .
+        . f e e e e e e e e e e e f . .
+        . . f e e e e e e e e e f . . .
+        . . . f f 5 5 5 5 5 f f . . . .
+        . . . . f f f f f f f . . . . .
+    `;
+
+    export const espresso = img`
+        f f f f f f f f f f f f f f f f
+        f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f
+        f 1 6 6 6 1 1 6 6 6 1 1 a a 1 f
+        f 1 6 9 6 1 1 6 9 6 1 1 a a 1 f
+        f 1 6 6 6 1 1 6 6 6 1 1 a a 1 f
+        f 1 1 1 1 1 1 1 1 1 1 1 a a 1 f
+        f f f f f f f f f f f f f f f f
+        f e e e e e e e e e e e e e e f
+        f e e 1 1 e e e e 1 1 e e e e f
+        f e 1 1 1 1 e e 1 1 1 1 e e e f
+        f e 1 1 1 1 e e 1 1 1 1 e e e f
+        f e e 1 1 e e e e 1 1 e e e e f
+        f e e e e e e e e e e e e e e f
+        f e e 5 5 5 5 5 5 5 5 e e e e f
+        f e e 5 5 5 5 5 5 5 5 e e e e f
+        f f f f f f f f f f f f f f f f
+    `;
+
+    export const v60 = img`
+        . . . . f f f f f f f . . . . .
+        . . . f 9 9 9 9 9 9 9 f . . . .
+        . . . f 9 1 9 9 9 1 9 f . . . .
+        . . . . f 9 9 9 9 9 f . . . . .
+        . . . . . f 9 9 9 f . . . . . .
+        . . . . . . f 5 f . . . . . . .
+        . . . . . . . f . . . . . . . .
+        . . . . f f f f f f f . . . . .
+        . . . f 9 9 9 9 9 9 9 f . . . .
+        . . . f 9 1 9 9 9 1 9 f . . . .
+        . . . f 9 9 9 9 9 9 9 f . . . .
+        . . . f 1 9 9 9 9 9 1 f . . . .
+        . . . f 9 9 9 9 9 9 9 f . . . .
+        . . . . f f 9 9 9 f f . . . . .
+        . . . . . f f f f f . . . . . .
+        . . . . . . . . . . . . . . . .
+    `;
+
+    export const floorTile = img`
+        d d d d d d d d d d f d d d d d
+        e e e e e e e e e e f e e e e e
+        e e e e e e e e e e f e e e e e
+        f f f f f f f f f f f f f f f f
+        d d d d d f d d d d d d d d d d
+        e e e e e f e e e e e e e e e e
+        e e e e e f e e e e e e e e e e
+        f f f f f f f f f f f f f f f f
+        d d d d d d d d d d d d f d d d
+        e e e e e e e e e e e e f e e e
+        e e e e e e e e e e e e f e e e
+        f f f f f f f f f f f f f f f f
+        d d d d d d d f d d d d d d d d
+        e e e e e e e f e e e e e e e e
+        e e e e e e e f e e e e e e e e
+        f f f f f f f f f f f f f f f f
+    `;
+
+    export const counterTile = img`
+        e e e e e e e e e e e e e e e e
+        e d d d d d d e e d d d d d d e
+        e d e e e e d e e d e e e e d e
+        e d e e e e d e e d e e e e d e
+        e d e e e e d e e d e e e e d e
+        e d e e e e d e e d e e e e d e
+        e d e e e e d e e d e e e e d e
+        e d e e e e d e e d e e e e d e
+        e d e e e e d e e d e e e e d e
+        e d e e e e d e e d e e e e d e
+        e d e e e e d e e d e e e e d e
+        e d d d d d d e e d d d d d d e
+        e e e e e e e e e e e e e e e e
+        e e e e e e e e e e e e e e e e
+        f f f f f f f f f f f f f f f f
+        f f f f f f f f f f f f f f f f
+    `;
+
+    export const beanBagMantiqueira = img`
+        . . f f f f f f f f f f f . . .
+        . f d d d d d d d d d d d f . .
+        f d d e d d d d d d d e d d f .
+        f d e e e e e e e e e e e d f .
+        f d e e e e e e e e e e e d f .
+        f d e e 6 6 6 6 6 6 6 e e d f .
+        f d e e 6 1 1 1 1 1 6 e e d f .
+        f d e e 6 1 1 1 1 1 6 e e d f .
+        f d e e 6 6 6 6 6 6 6 e e d f .
+        f d e e e e e e e e e e e d f .
+        f d e e e e e e e e e e e d f .
+        f d e e e e e e e e e e e d f .
+        f d d e d d d d d d d e d d f .
+        . f d d d d d d d d d d d f . .
+        . . f f f f f f f f f f f . . .
+        . . . . . . . . . . . . . . . .
+    `;
+
+    export const beanBagColombian = img`
+        . . f f f f f f f f f f f . . .
+        . f d d d d d d d d d d d f . .
+        f d d e d d d d d d d e d d f .
+        f d e e e e e e e e e e e d f .
+        f d e e e e e e e e e e e d f .
+        f d e e 2 4 4 4 4 4 2 e e d f .
+        f d e e 4 1 1 1 1 1 4 e e d f .
+        f d e e 4 1 1 1 1 1 4 e e d f .
+        f d e e 2 4 4 4 4 4 2 e e d f .
+        f d e e e e e e e e e e e d f .
+        f d e e e e e e e e e e e d f .
+        f d e e e e e e e e e e e d f .
+        f d d e d d d d d d d e d d f .
+        . f d d d d d d d d d d d f . .
+        . . f f f f f f f f f f f . . .
+        . . . . . . . . . . . . . . . .
+    `;
+}

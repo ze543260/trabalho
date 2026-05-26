36d814095b58b5b9dc03f883061de337d9c9e30b
Ze543260 <josevitorg170@gmail>
2026-05-25 21:46:43 -0300

    fix

--- main.ts
+++ main.ts
@@ -1,2 +1,9 @@
 Engine.Core.init();
+
+// A peça que faltava: Conectar o loop do Kernel à Pilha de Cenas!
+Engine.Core.registerUpdate(function(dt: number) {
+    Engine.Scenes.SceneStack.update(dt);
+});
+
+// Dá o arranque ao jogo
 Engine.Scenes.SceneStack.push(new Engine.Scenes.GameplayScene());
